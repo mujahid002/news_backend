952c4bf3b4f9fd10e5225193dbdbbc6618ece469
Mujahid002 <mujahidshaik2002@gmail.com>
2024-02-29 16:44:25 +0530

    backend server on 5555

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connectMongo = require("./database/connectMongo");
+// const router = require("./components/routers/index.js")
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// app.use('/v1', router)
+
+app.get("/", (req, res) => {
+  res.send("Hello Iam running on 5555!");
+});
+
+// Start the server and listen on port 5555
+app.listen(5555, () => {
+  console.log("App listening on port 5555");
+  connectMongo();
+});
