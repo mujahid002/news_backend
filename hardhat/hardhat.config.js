d1ee70a4a3b7b830456761b47222cdef2c513e5b
Mujahid002 <mujahidshaik2002@gmail.com>
2024-03-02 19:42:15 +0530

    updated configuration

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,31 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@openzeppelin/hardhat-upgrades");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    mumbai: {
+      url: process.env.AlCHEMY_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 80001,
+      // gasPrice: 25000000000,
+      saveDeployments: true,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.POLYGON_API_KEY,
+    },
+  },
+  sourcify: {
+    enabled: true,
+  },
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 1000,
+    },
+  },
 };
