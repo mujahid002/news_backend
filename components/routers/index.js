9bd803ffb66116d94be12ae1830b65666258c17a
Mujahid002 <mujahidshaik2002@gmail.com>
2024-03-07 14:30:46 +0530

    added more routes

--- components/routers/index.js
+++ components/routers/index.js
@@ -3,11 +3,13 @@ const connectMongo = require("../../database/connectMongo.js");
 const {
   fetchOrganisationsDetails,
   fetchJournalistsDetails,
+  fetchNews,
 } = require("../../database/functions/index.js");
 const {
   loginController,
   orgRegisterController,
   journalistRegisterController,
+  submitNewsController,
 } = require("../controllers/index.js");
 const {
   LoginSchema,
@@ -15,6 +17,7 @@ const {
   JournalistRegistrationSchema,
   OrgLoginSchema,
   JournalistLoginSchema,
+  NewsDetailsSchema,
 } = require("../validators/index.js");
 const validateSchema = require("../validators/validateSchema.js");
 
@@ -25,32 +28,32 @@ apiRouter.post(
   // validateSchema(LoginSchema),
   loginController
 );
-apiRouter.get(
-  "/login",
-  // validateSchema(LoginSchema),
-  loginController
-);
 apiRouter.post(
   "/org/register",
   validateSchema(OrgRegistrationSchema),
   orgRegisterController
 );
-apiRouter.post( 
+apiRouter.post(
   "/journalist/register",
   validateSchema(JournalistRegistrationSchema),
   journalistRegisterController
 );
-apiRouter.get(
-  "/org/register",
-  // validateSchema(OrgRegistrationSchema),
-  orgRegisterController
+apiRouter.post(
+  "/news/submit-news",
+  validateSchema(NewsDetailsSchema),
+  submitNewsController
 );
-apiRouter.get("/orgs", fetchOrganisationsDetails);
-apiRouter.get("/journalists", fetchJournalistsDetails);
-apiRouter.get(
-  "/journalist/register",
-  // validateSchema(JournalistRegistrationSchema),
-  journalistRegisterController
-);
+apiRouter.get("/organizations", async (req, res) => {
+  const data = await fetchOrganisationsDetails();
+  res.status(200).json({ organizations: data });
+});
+apiRouter.get("/journalists", async (req, res) => {
+  const data = await fetchJournalistsDetails();
+  res.status(200).json({ journalists: data });
+});
+apiRouter.get("/news", async (req, res) => {
+  const data = await fetchNews();
+  res.status(200).json({ news: data });
+});
 
 module.exports = apiRouter;
