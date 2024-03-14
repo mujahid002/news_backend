25f9d5ed1800f25b95a8f02b0ccb4526ae39ad98
Mujahid002 <mujahidshaik2002@gmail.com>
2024-03-14 20:47:07 +0530

    updated contract with other functionalities

--- hardhat/contracts/TestNews.sol
+++ hardhat/contracts/TestNews.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {ERC721, IERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+// import {ERC721, IERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721, ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {IERC721} from "@openzeppelin/contracts/interfaces/IERC721.sol";
 import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import {ERC721Burnable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
@@ -16,32 +17,46 @@ error News__UserCannotBurnCertificate();
 error News__UserCannotTransferCertificate();
 
 // TestWallet Contract
-contract TestNews is ERC721Enumerable, ERC721URIStorage, ERC721Burnable, AccessControl, Pausable {
+contract TestNews is
+    ERC721Enumerable,
+    ERC721URIStorage,
+    ERC721Burnable,
+    AccessControl,
+    Pausable
+{
     // Bytes32 for Access Control Roles
     bytes32 public constant NEWS_PUBLISHER_ROLE =
         keccak256("NEWS_PUBLISHER_ROLE");
+    bytes32 public constant NEWS_FACT_CHECKER_ROLE =
+        keccak256("NEWS_FACT_CHECKER_ROLE");
 
-    // Mapping: tokenId is mapped with Array of Bytes, array elements indicates CID in bytes format
-    mapping(uint256 => bytes[]) private s_newsData;
+    // Mapping: tokenId is mapped with Array of Strings, array elements indicates CID
+    mapping(uint256 => string[]) private s_newsData;
+
+    // Mapping: string is mapped with Array of Strings, array elements indicates fect checked CID for a particular CID
+    mapping(string => string[]) private s_factCheckerNews;
+
+    // Mapping: tokenId is mapped with Array of journalist tokenIds
+    mapping(uint256 => uint256[]) private s_publishersInOrg;
 
     // events
-    event storedLatestNews(
+    event newsCidEvent(
         uint256 indexed tokenId,
         string indexed cid,
-        bytes indexed data
+        string indexed factCid
     );
     // Event for token transfers
     event transfer(
         address indexed from,
         address indexed to,
-        uint256 indexed tokenId,
-        bytes data
+        uint256 indexed tokenId
     );
 
     // Constructor
     constructor() ERC721("TestNewsToken", "TNT") {
         _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
         _grantRole(NEWS_PUBLISHER_ROLE, _msgSender());
+        _grantRole(NEWS_FACT_CHECKER_ROLE, _msgSender());
     }
 
     /**************************
@@ -60,15 +75,16 @@ contract TestNews is ERC721Enumerable, ERC721URIStorage, ERC721Burnable, AccessC
 
         return _allUserTokenIds;
     }
+
     function fetchLatestTokenIdOfAnUser(address _userAddress)
         public
         view
         returns (uint256)
     {
         uint256 userTokenIdCount = balanceOf(_userAddress);
-        return tokenOfOwnerByIndex(_userAddress, userTokenIdCount-1);
-        
+        return tokenOfOwnerByIndex(_userAddress, userTokenIdCount - 1);
     }
+
     /**************************
         MAIN FUNCTIONS
     ***************************/
@@ -76,54 +92,123 @@ contract TestNews is ERC721Enumerable, ERC721URIStorage, ERC721Burnable, AccessC
     /**
      * @dev Send new Certificate to a user with the specified URI.
      * @param _to: Address of the publisher.
-     * @param _credsURI: URI indicates Certificate for Publisher.
+     * @param _tokenURI: URI indicates Certificate for Publisher.
      */
-    function sendCertificate(
+    function issueCertificate(
         address _to,
+        bool IsfactChecker,
         string calldata userName,
-        string calldata _credsURI
+        string calldata _tokenURI
     ) public whenNotPaused onlyRole(DEFAULT_ADMIN_ROLE) {
         // for testing purposes removed check...
         // if (checkAddressHaveCertificate(_to))
         //     revert News__AlreadyUserHaveCertificate();
         uint256 tokenId = generateTokenId(_to, userName);
         _safeMint(_to, tokenId);
-        _setTokenURI(tokenId, _credsURI);
-        _grantRole(NEWS_PUBLISHER_ROLE, _to);
-        emit transfer(_msgSender(),_to,tokenId,"");
+        _setTokenURI(tokenId, _tokenURI);
+        if (IsfactChecker) {
+            _grantRole(NEWS_FACT_CHECKER_ROLE, _to);
+        } else {
+            _grantRole(NEWS_PUBLISHER_ROLE, _to);
+        }
+        emit transfer(_msgSender(), _to, tokenId);
     }
 
     /**
      * @dev Burn a tokenId belonging to the specified tokenId.
-     * @param tokenId: ID of the publisher to burn.
+     * @param tokenId: ID of the user to burn.
      */
-    function burnCertificate(uint256 tokenId)
+    function revokeCertificate(uint256 tokenId, bool IsfactChecker)
         public
         whenNotPaused
         onlyRole(DEFAULT_ADMIN_ROLE)
     {
-        if (ownerOf(tokenId) == address(0)) revert News__TokenIdDoesNotExists();
-
-        _burn(tokenId);
-        _revokeRole(NEWS_PUBLISHER_ROLE, ownerOf(tokenId));
+        address userAddress = ownerOf(tokenId);
+        if (userAddress == address(0)) revert News__TokenIdDoesNotExists();
+        if (IsfactChecker) {
+            if (!hasRole(NEWS_FACT_CHECKER_ROLE, userAddress)) revert();
+            _burn(tokenId);
+            _revokeRole(NEWS_FACT_CHECKER_ROLE, ownerOf(tokenId));
+        } else {
+            if (!hasRole(NEWS_PUBLISHER_ROLE, userAddress)) revert();
+            _burn(tokenId);
+            _revokeRole(NEWS_PUBLISHER_ROLE, ownerOf(tokenId));
+        }
     }
 
     /**
      * @dev Store news content as a CID by Publisher.
      * @param newsCid: CID of the news from IPFS to be stored on contract.
      */
-    function submitNews(string calldata newsCid)
+    function notorize(string calldata newsCid)
         public
         whenNotPaused
         onlyRole(NEWS_PUBLISHER_ROLE)
     {
         uint256 tokenId = getTokenIdOfAnUser(_msgSender());
 
-        // Convert newsCid string to bytes
-        bytes memory cidInBytes = bytes(newsCid);
+        s_newsData[tokenId].push(newsCid);
+        emit newsCidEvent(tokenId, newsCid, "");
+    }
 
-        s_newsData[tokenId].push(cidInBytes);
-        emit storedLatestNews(tokenId, newsCid, cidInBytes);
+    /**
+     * @dev Store news content as a CID by Publisher.
+     * @param newsCid: CID of the news from IPFS to be stored on contract.
+     * @param factNewsCid: CID of the fact checker news from IPFS to be stored on contract.
+     */
+
+    function factChecker(string calldata newsCid, string calldata factNewsCid)
+        public
+        whenNotPaused
+        onlyRole(NEWS_FACT_CHECKER_ROLE)
+    {
+        uint256 tokenId = getTokenIdOfAnUser(_msgSender());
+
+        s_factCheckerNews[newsCid].push(factNewsCid);
+
+        emit newsCidEvent(tokenId, newsCid, factNewsCid);
+    }
+
+    /**
+     * @dev verify the news cid from tokenId and cid .
+     * @param tokenId: Token Id of the user.
+     * @param newsCid: CID of the news from IPFS to be stored on contract.
+     */
+
+    function verifyCid(uint256 tokenId, string calldata newsCid)
+        public
+        view
+        returns (bool)
+    {
+        string[] memory cidArray = getAllNewsCids(tokenId);
+        if (cidArray.length == 0) revert();
+        bytes32 newsCidInBytes32 = keccak256(abi.encodePacked(newsCid));
+        for (uint256 i = 0; i < cidArray.length; ++i) {
+            if (newsCidInBytes32 == keccak256(abi.encodePacked(cidArray[i]))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function verifyFactCheckerCid(
+        string calldata newsCid,
+        string calldata factCheckerCid
+    ) public view returns (bool) {
+        string[] memory cidArray = getAllFactCheckCids(newsCid);
+        if (cidArray.length == 0) revert();
+        bytes32 factCheckerCidInBytes32 = keccak256(
+            abi.encodePacked(factCheckerCid)
+        );
+        for (uint256 i = 0; i < cidArray.length; ++i) {
+            if (
+                factCheckerCidInBytes32 ==
+                keccak256(abi.encodePacked(cidArray[i]))
+            ) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**************************
@@ -161,46 +246,23 @@ contract TestNews is ERC721Enumerable, ERC721URIStorage, ERC721Burnable, AccessC
         );
 
         // Convert the concatenated string to uint256
-        return stringToUint256(concatenatedString);
+        return uint256(keccak256(bytes(concatenatedString)));
     }
 
-    /**
-     * @dev Fetch news data associated with a specific tokenId.
-     * @param tokenId: The unique identifier of publisher.
-     */
-    function fetchNewsForTokenIdInStringArray(uint256 tokenId)
+    function getLatestCid(uint256 tokenId) public view returns (string memory) {
+        string[] memory cidArray = getAllNewsCids(tokenId);
+        if (cidArray.length == 0) revert();
+        return cidArray[cidArray.length - 1];
+    }
+
+    function getLatestfactCheckerCid(string calldata newsCid)
         public
         view
-        returns (string[] memory)
+        returns (string memory)
     {
-        // Retrieve news data in bytes array associated with the given tokenId
-        bytes[] memory allCidInBytesArray = s_newsData[tokenId];
-        string[] memory allCidInStringArray = new string[](
-            allCidInBytesArray.length
-        );
-
-        // Convert bytes to string from each element to CID(string)
-        for (uint256 i = 0; i < allCidInBytesArray.length; ++i) {
-            allCidInStringArray[i] = string(
-                abi.encodePacked(allCidInBytesArray[i])
-            );
-        }
-        return allCidInStringArray;
-    }
-
-    /**
-     * @dev Get the latest CID associated with a specific tokenId.
-     * @param tokenId: The unique identifier of publisher.
-     */
-    function getLatestCid(uint256 tokenId) public view returns (string memory) {
-        bytes[] memory allCidInBytesArray = s_newsData[tokenId];
-        uint256 cidArrayLength = allCidInBytesArray.length;
-
-        // Get the latest content identifier in bytes
-        bytes memory cidInBytes = allCidInBytesArray[cidArrayLength - 1];
-
-        // Convert bytes to string for the latest content identifier
-        return string(abi.encodePacked((cidInBytes)));
+        string[] memory cidArray = getAllFactCheckCids(newsCid);
+        if (cidArray.length == 0) revert();
+        return cidArray[cidArray.length - 1];
     }
 
     /**
@@ -219,49 +281,22 @@ contract TestNews is ERC721Enumerable, ERC721URIStorage, ERC721Burnable, AccessC
      * @dev Get the news data (content identifiers) associated with a specific tokenId.
      * @param tokenId: The unique identifier of publisher.
      */
-    function getNewsDataInBytesArray(uint256 tokenId) public view returns (bytes[] memory) {
+    function getAllNewsCids(uint256 tokenId)
+        public
+        view
+        returns (string[] memory)
+    {
         // Retrieve news data in bytes associated with the given tokenId
         return s_newsData[tokenId];
     }
 
-    /**
-     * @dev Check if a user owns any Certificate.
-     * @param _userAddress: Address of the user.
-     */
-    function checkAddressHaveCertificate(address _userAddress)
+    function getAllFactCheckCids(string calldata newsCid)
         public
         view
-        returns (bool)
+        returns (string[] memory)
     {
-        // for testing...
-        return balanceOf(_userAddress) > 0;
-
-        // return balanceOf(_userAddress)==1;
-    }
-
-    /**************************
-        HELPER PURE FUNCTIONS
-    ***************************/
-
-    /**
-     * @dev Convert a string to uint256 using keccak256 hash.
-     * @param theString: String to convert.
-     */
-    function stringToUint256(string memory theString)
-        public
-        pure
-        returns (uint256)
-    {
-        // Convert the string to bytes
-        bytes memory theBytes = bytes(theString);
-
-        // Calculate the keccak256 hash of the bytes
-        bytes32 theHash = keccak256(theBytes);
-
-        // Convert the hash to uint256
-        uint256 theUint256 = uint256(theHash);
-
-        return theUint256;
+        // Retrieve fact check news data associated with the given newsCid
+        return s_factCheckerNews[newsCid];
     }
 
     /**************************
@@ -277,8 +312,9 @@ contract TestNews is ERC721Enumerable, ERC721URIStorage, ERC721Burnable, AccessC
         uint256 tokenId,
         bytes memory data
     ) internal virtual override {
-        emit transfer(from, to, tokenId, data);
-        revert News__UserCannotTransferCertificate();
+        if (_msgSender() != address(0))
+            revert News__UserCannotTransferCertificate();
+        super._safeTransfer(from, to, tokenId, data);
     }
 
     function safeTransferFrom(
@@ -287,8 +323,9 @@ contract TestNews is ERC721Enumerable, ERC721URIStorage, ERC721Burnable, AccessC
         uint256 tokenId,
         bytes memory data
     ) public virtual override(ERC721, IERC721) {
-        emit transfer(from, to, tokenId, data);
-        revert News__UserCannotTransferCertificate();
+        if (_msgSender() != address(0))
+            revert News__UserCannotTransferCertificate();
+        super.safeTransferFrom(from, to, tokenId, data);
     }
 
     function transferFrom(
@@ -296,8 +333,9 @@ contract TestNews is ERC721Enumerable, ERC721URIStorage, ERC721Burnable, AccessC
         address to,
         uint256 tokenId
     ) public virtual override(ERC721, IERC721) {
-        emit transfer(from, to, tokenId, "");
-        revert News__UserCannotTransferCertificate();
+        if (_msgSender() != address(0))
+            revert News__UserCannotTransferCertificate();
+        super.transferFrom(from, to, tokenId);
     }
 
     /**************************
@@ -335,7 +373,7 @@ contract TestNews is ERC721Enumerable, ERC721URIStorage, ERC721Burnable, AccessC
         public
         view
         virtual
-        override(ERC721, ERC721Enumerable, ERC721URIStorage,AccessControl)
+        override(ERC721, ERC721Enumerable, ERC721URIStorage, AccessControl)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
